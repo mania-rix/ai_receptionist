97673b854070e25098d0941e35ea5440e60161bd
Amir <125951876+mania-rix@users.noreply.github.com>
2025-06-09 17:06:05 -0400

    Updated page.tsx

--- app/auth/callback/page.tsx
+++ app/auth/callback/page.tsx
@@ -2,7 +2,7 @@
 
 import { useEffect } from 'react';
 import { useRouter } from 'next/navigation';
-import { createBrowserSupabaseClient } from '@/lib/supabase';
+import { supabaseBrowser } from '@/lib/supabase'
 
 export default function AuthCallbackPage() {
   const router = useRouter();
