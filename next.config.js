b7f373df1536adcdfda8e3706fd8fb67fdca8fc8
Amir <125951876+mania-rix@users.noreply.github.com>
2025-06-09 15:48:21 -0400

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -7,3 +7,16 @@ const nextConfig = {
 };
 
 module.exports = nextConfig;
+// next.config.js
+module.exports = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        bufferutil: false,
+        "utf-8-validate": false,
+      };
+    }
+    return config;
+  },
+};
