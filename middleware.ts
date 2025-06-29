916f88041bbfd6160df453334ca3e328c918b8ce
Amir <125951876+mania-rix@users.noreply.github.com>
2025-06-29 14:57:05 -0400

    Deleted middleware.ts

--- middleware.ts
+++
@@ -1,5 +0,0 @@
-// This file is disabled in demo mode
-// All authentication is handled client-side with sessionStorage
-
-// In demo mode, all authentication is handled client-side
-// This middleware is disabled
