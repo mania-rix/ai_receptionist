d8d252844543a7892eb08853e4b774bad62741c9
Amir <125951876+mania-rix@users.noreply.github.com>
2025-06-29 23:42:14 -0400

    Updated route.ts

--- app/api/phone-numbers/route.ts
+++ app/api/phone-numbers/route.ts
@@ -16,7 +16,7 @@ export async function GET() {
        // process.env.NEXT_PUBLIC_SUPABASE_URL!,
        // process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!
      // );
- //   }
+    }
 
     const { data: phoneNumbers, error } = await supabase
       .from('phone_numbers')
