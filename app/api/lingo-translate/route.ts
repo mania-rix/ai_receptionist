d07afff7e8eee1e0ee3670f5ca9028ac4157c6d3
Amir <125951876+mania-rix@users.noreply.github.com>
2025-06-30 05:40:24 -0400

    Updated route.ts

--- app/api/lingo-translate/route.ts
+++ app/api/lingo-translate/route.ts
@@ -86,8 +86,8 @@ export async function GET(req: Request) {
     let user;
     
     try {
-      const cookieStore = cookies();
-      supabase = supabaseServer(cookieStore);
+    //  const cookieStore = cookies();
+      supabase = supabaseServer();
       const { data: { user: authUser } } = await supabase.auth.getUser();
       user = authUser;
     } catch (error) {
